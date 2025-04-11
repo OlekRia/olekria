0bf8a506f0ffb4436ae2d9bfc738b2ac19e6a81b
oriabukha <alerya@gmail.com>
2025-04-11 20:31:08 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olekrial.github.io',
-  base: 'wbc',
   vite: {
     plugins: [tailwindcss()],
   },
